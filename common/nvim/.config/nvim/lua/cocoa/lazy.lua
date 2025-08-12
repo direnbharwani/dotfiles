fa4319be575e0e81bc25e50c28d86024d4021d98
Diren <diren.bharwani@gmail.com>
2025-08-12 11:17:33 +0800

    Updated neovim dotfiles; cleaned up lsp setup

--- common/nvim/.config/nvim/lua/cocoa/lazy.lua
+++ common/nvim/.config/nvim/lua/cocoa/lazy.lua
@@ -1,43 +1,42 @@
 -- [[ Boostrap lazy.nvim: A Neovim Plugin Manager]]
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Checks for existence of lazy.nvim installed
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none', -- partial clone
-    '--branch=stable', -- Latest Stable Release
-    lazyrepo,
-    lazypath,
-  })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none", -- partial clone
+		"--branch=stable", -- Latest Stable Release
+		lazyrepo,
+		lazypath,
+	})
 
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-  end
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+	end
 end ---@diagnostic disable-next-line: undefined-field
 
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
-require('lazy').setup({
-  { import = 'cocoa.plugins' },
-  { import = 'cocoa.plugins.git' },
-  { import = 'cocoa.plugins.lsp' },
-  { import = 'cocoa.plugins.markdown' },
-  { import = 'cocoa.plugins.themes' },
-  { import = 'cocoa.plugins.utils' },
+require("lazy").setup({
+	{ import = "cocoa.plugins" },
+	{ import = "cocoa.plugins.git" },
+	{ import = "cocoa.plugins.markdown" },
+	{ import = "cocoa.plugins.themes" },
+	{ import = "cocoa.plugins.utils" },
 }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
