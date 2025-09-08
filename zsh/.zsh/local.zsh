e65e2c47c67e67590a3e67cd2d1fc3ef51451e5d
Diren D Bharwani <50872976+direnbharwani@users.noreply.github.com>
2025-09-08 18:40:24 +0800

    Delete zsh/.zsh/local.zsh

--- zsh/.zsh/local.zsh
+++
@@ -1,7 +0,0 @@
-# Private/internal tool completions
-# This file should be gitignored and not committed to public repos
-
-# Internal tool completions
-source <(capri --zsh-completions 2>/dev/null)
-source <(isc --zsh-completions 2>/dev/null)
-source <(acc --zsh-completions 2>/dev/null)
