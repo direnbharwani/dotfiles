ea2d588e2ae78a35a8b51b72b708248d0787e8b3
Diren <diren.bharwani@gmail.com>
2026-02-09 13:07:34 +0800

    Update nvim

--- nvim/.config/nvim/lua/cocoa/plugins/mini.lua
+++ nvim/.config/nvim/lua/cocoa/plugins/mini.lua
@@ -1,40 +1,44 @@
 return {
-  -- mini.icons
-  {
-    'echasnovski/mini.icons',
-    version = '*',
-    opts = {},
-  },
-  -- mini.comments
-  {
-    'echasnovski/mini.comment',
-    version = '*',
-    opts = {},
-  },
-  -- mini.tabline
-  {
-    'echasnovski/mini.tabline',
-    version = '*',
-    opts = {
-      show_icons = true,
-    },
-  },
-  -- mini.ai
-  {
-    'echasnovski/mini.ai',
-    version = '*',
-    opts = {},
-  },
-  -- mini.surround
-  {
-    'echasnovski/mini.surround',
-    version = '*',
-    opts = {},
-  },
-  -- mini.pairs
-  {
-    'echasnovski/mini.pairs',
-    version = '*',
-    opts = {},
-  },
+	-- mini.icons
+	{
+		"echasnovski/mini.icons",
+		version = "*",
+		opts = {},
+	},
+	-- mini.comments
+	{
+		"echasnovski/mini.comment",
+		version = "*",
+		opts = {},
+	},
+	-- mini.tabline
+	{
+		"echasnovski/mini.tabline",
+		version = "*",
+		opts = {
+			show_icons = true,
+			format = function(buf_id, label)
+				local suffix = vim.bo[buf_id].modified and " [+]" or ""
+				return MiniTabline.default_format(buf_id, label) .. suffix
+			end,
+		},
+	},
+	-- mini.ai
+	{
+		"echasnovski/mini.ai",
+		version = "*",
+		opts = {},
+	},
+	-- mini.surround
+	{
+		"echasnovski/mini.surround",
+		version = "*",
+		opts = {},
+	},
+	-- mini.pairs
+	{
+		"echasnovski/mini.pairs",
+		version = "*",
+		opts = {},
+	},
 }
--- nvim/.config/nvim/lua/cocoa/plugins/themes/nord.lua
+++ nvim/.config/nvim/lua/cocoa/plugins/themes/nord.lua
@@ -1,9 +1,5 @@
 return {
-	"gbprod/nord.nvim",
+	"shaunsingh/nord.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("nord").setup({})
-		vim.cmd.colorscheme("nord")
-	end,
 }
